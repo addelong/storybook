53ebfe7e464787d4752ec5b239b88fc3b137997e
addelong <alan.delong@ceros.com>
2024-02-14 02:03:48 -0500

    working shorts generator

--- dialog.py
+++ dialog.py
@@ -1,18 +1,16 @@
 import aiohttp
 import os
-from creds import elevenlabs_api_key, voice_model_id
 
-
-async def get_dialog_tracks(text_chunks):
+async def get_dialog_tracks(text_chunks, elevenlabs_api_key, voice_model_id):
     dir_path = "./out/dialog/"
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
 
     for idx, text_chunk in enumerate(text_chunks):
-        await generate_dialog_track(text_chunk, idx)
+        await generate_dialog_track(text_chunk, idx, elevenlabs_api_key, voice_model_id)
 
 
-async def generate_dialog_track(text_chunk, idx):
+async def generate_dialog_track(text_chunk, idx, elevenlabs_api_key, voice_model_id):
     # Set the API endpoint URL
     url = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_model_id}"
 
@@ -23,6 +21,8 @@ async def generate_dialog_track(text_chunk, idx):
         "Content-Type": "application/json",
         "xi-api-key": elevenlabs_api_key
     }
+
+    print(f"Making request to url: {url} with api key: {elevenlabs_api_key} and voice model id: {voice_model_id}")
 
     # Set the request payload
     payload = {
@@ -41,6 +41,7 @@ async def generate_dialog_track(text_chunk, idx):
         await session.close()
         
     else:
+        print(response.text)
         # Raise an exception if the request failed
         raise Exception("Failed to generate dialog track: {}".format(response.text))
         
