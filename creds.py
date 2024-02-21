e7d668d8c911d8b1a6937e398253f6f19e90adba
addelong <alan.delong@ceros.com>
2024-02-21 18:33:18 -0500

    cfg scale adjustment

--- creds.py
+++ creds.py
@@ -1,4 +1,4 @@
 openai_api_key = ""
-stability_api_key = ""
-elevelabs_api_key = ""
-voice_model_id = ""
+stability_api_key = "sk-CEhotW0MYUFyccB4fSNZYVkNd41Jf8lpIk4OotgEyymiWv0t"
+elevenlabs_api_key = "adb589c7df42d038f4516f83ad2943d8"
+voice_model_id = "OD3nTCthkC5vBHyQp1RF"
